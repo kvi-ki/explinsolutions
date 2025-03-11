1c287e6f19d8fc0e288e110c74b3bddf83a84a94
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-03-11 14:12:29 +0100

    chore: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body className="font-sans" id="root">
         {children}
       </body>
