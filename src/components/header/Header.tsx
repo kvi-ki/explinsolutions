dce18b5b9759d24f2aea7a9d563b2de73746a012
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-03-13 17:18:17 +0100

    style: update tailwind styles for header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,20 +4,14 @@ export default function Header() {
       <svg width="0" height="0">
         <defs>
           <clipPath id="headerClip" clipPathUnits="objectBoundingBox">
-            <path d="M 0,0 L 1,0 L 1,0.70 L 0.55,0.70 0.52,0.73 L 0.52,1 L 0,1 L 0,0 Z" />
+            <path d="M 0,0 L 1,0 L 1,0.70 L 0.52,0.70 L 0.52,1 L 0,1 L 0,0 Z" />
           </clipPath>
         </defs>
       </svg>
 
       <header
-        className="m-3 min-h-[250px] text-white"
-        style={{
-          backgroundImage: "url('/assets/electric_tower.jpg')",
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          clipPath: 'url(#headerClip)',
-          borderRadius: '3%'
-        }}
+        className="m-3 min-h-[250px] text-white bg-[url(/assets/electric_tower.jpg)] bg-cover bg-center rounded-xl"
+        style={{ clipPath: 'url(#headerClip)' }}
       >
         <h1 className="relative top-2 left-4 text-header tracking-wide">
           Explinsolutions
