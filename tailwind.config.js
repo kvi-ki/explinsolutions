eb837a7933a607eba2dfbab0e26a00e9ac17bf64
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-03-18 14:45:31 +0100

    style: improving styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,20 @@ export default {
         body: ["Roboto", "sans-serif"]
       },
       fontSize: {
+        heading: 'clamp(1.2rem, 2.5% + 1vw, 4rem)',
         mdPreamble: 'clamp(0.6rem, 0.8rem + 1vw, 3rem)'
       },
       spacing: {
-        topSpacing: 'clamp(24rem, 25rem + 1vw, 30rem)'
+        topSx: 'clamp(11.3rem, 1.5% + 1vw, 14rem)',
+        topSm: 'clamp(12rem, 1.5% + 1vw, 14rem)',
+        topMd: 'clamp(16rem, 2.5% + 1vw, 23rem)',
+        topLg: 'clamp(24rem, 2.5% + 1vw, 25rem)',
+        topXl: 'clamp(25rem, 5% + 1vw, 26rem)',
+        rightSx: 'clamp(1rem, 1% + 1vw, 1rem)',
+        rightSm: 'clamp(1.5rem, 1% + 1vw, 7rem)',
+        rightMd: 'clamp(2rem, 2% + 3vw, 10rem)',
+        rightLg: 'clamp(12rem, 2% + 1vw, 24rem)',
+        rightXl: 'clamp(4rem, 2% + 3vw, 14rem)',
       }
     },
     colors: {
