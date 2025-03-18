89f32000c89291e396aa8de1bebc793d4af6de93
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-03-18 12:14:29 +0100

    feat: control image path while resizing

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
+
 export default function Header() {
+  const screenPath = 'M 0,0 L 1,0 L 1,0.70 L 0.52,0.70 L 0.52,1 L 0,1 L 0,0 Z';
+  const largeScreenPath =
+    'M 0,0 L 1,0 L 1,0.70 L 0.60,0.70 L 0.60,1 L 0,1 L 0,0 Z';
+
+  const [clipPath, setClipPath] = useState(screenPath);
+
+  useEffect(() => {
+    const updateClipPath = () => {
+      setClipPath(window.innerWidth > 1280 ? largeScreenPath : screenPath);
+    };
+
+    updateClipPath();
+    window.addEventListener('resize', updateClipPath);
+
+    return () => window.removeEventListener('resize', updateClipPath);
+  }, []);
+
   return (
     <>
       <svg width="0" height="0">
         <defs>
           <clipPath id="headerClip" clipPathUnits="objectBoundingBox">
-            <path d="M 0,0 L 1,0 L 1,0.70 L 0.52,0.70 L 0.52,1 L 0,1 L 0,0 Z" />
+            <path d={clipPath} />
           </clipPath>
         </defs>
       </svg>
@@ -16,6 +35,10 @@ export default function Header() {
           style={{ clipPath: 'url(#headerClip)' }}
         >
           <h1 className="heading">Explinsolutions_</h1>
+        </div>
+        <div className="preamble">
+          <p className="self-start">Servicio integral </p>
+          <p className="self-end">para la industria</p>
         </div>
       </header>
     </>
