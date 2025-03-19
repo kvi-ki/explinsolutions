64ba18ea505e50b8b3f9fb7e63d0b0eef94255b4
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-03-19 14:45:27 +0100

    feat: add maping for services

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,16 +1,19 @@
 import Service from './Service';
+import services from '../../data.json';
 
 export default function Services() {
   return (
-    <section>
-      <h2 className="text-center text-gray text-sectionHeader md:text-sectionHeaderMd lg:text-sectionHeaderLg">
+    <section className="bg-lightGray p-4">
+      <h2 className="pt-6 text-center text-gray text-sectionHeader md:text-sectionHeaderMd lg:text-sectionHeaderLg">
         <span className="font-extrabold text-black">
           Servicio de confianza,
         </span>
         &nbsp; para sus diversas necesidades
       </h2>
-      <ul className="m-[6vw]">
-        <Service />
+      <ul className="m-[6vw] flex flex-wrap justify-around align-center content-evenly">
+        {services.map((service) => (
+          <Service key={service.name} name={service.name} />
+        ))}
       </ul>
     </section>
   );
