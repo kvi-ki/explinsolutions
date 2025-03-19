20e06018f67569bb5dbc4b61e93848c0749f5cac
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-03-19 15:28:37 +0100

    style: add background color for Service square

--- src/components/services/Service.tsx
+++ src/components/services/Service.tsx
@@ -1,6 +1,6 @@
 export default function Service({ name }: { name: string }) {
   return (
-    <li className="border border-green rounded-xl p-2 m-4 w-1/3 aspect-square flex flex-col justify-center sm:1/6 md:w-1/6 xl:w-48">
+    <li className="bg-white border border-green rounded-xl p-2 m-4 w-1/3 aspect-square flex flex-col justify-center sm:1/6 md:w-1/6 xl:w-48">
       <h3 className="w-full text-center text-wrap">{name}</h3>
       <ul className="list-disc hidden">
         <li>Automatización Industrial</li>
