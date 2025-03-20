4d7fd5bc22c180d9d10fe268d92d51d6fd50ab87
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-03-20 11:44:08 +0100

    style: add spacing for services

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -4,10 +4,11 @@ import services from '../../data.json';
 export default function Services() {
   return (
     <section className="bg-lightGray p-4">
-      <h2 className="pt-6 text-center text-gray text-sectionHeader md:text-sectionHeaderMd lg:text-sectionHeaderLg">
+      <h2 className="pt-6 text-center text-gray text-sectionHeader md:text-sectionHeaderMd md:pt-10 lg:text-sectionHeaderLg lg:pt-20">
         <span className="font-extrabold text-black">
           Servicio de confianza,
         </span>
+        <br />
         &nbsp; para sus diversas necesidades
       </h2>
       <ul className="m-[6vw] flex flex-wrap justify-around align-center content-evenly">
