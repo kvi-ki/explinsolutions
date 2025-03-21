5bf5a3f87c37104369506838127507ca2707aa66
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-03-21 13:30:52 +0100

    feat: add footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,42 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhoneFlip, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
+export default function Footer() {
+  return (
+    <footer className="h-96 pt-12 pl-4 pr-4 pb-12 bg-darkGray grid md:grid-cols-2 gap-x-8 gap-y-8 md:pl-8 md:pr-8">
+      <section className="w-52 flex justify-between items-center">
+        <img
+          src="/favicon.png"
+          alt="Explinsolutions label"
+          className="size-6"
+        />
+        <p className="text-white font-extralight tracking-wide text-2xl">
+          Explinsolutions
+        </p>
+      </section>
+
+      <section className="text-sm text-white font-extralight flex flex-col gap-4 justify-center md:items-end">
+        <p>
+          Camí del Mig 5, 08349
+          <br />
+          Cabrera de Mar (BCN)
+        </p>
+        <p>+ 34 619 792 023</p>
+        <p>administracion@explinsolutions.com</p>
+      </section>
+
+      <section className="flex items-center gap-4 md:col-span-2 md:justify-end">
+        <a href="mailto:administracion@explinsolutions.com">
+          <FontAwesomeIcon icon={faEnvelope} className="footer-icon" />
+        </a>
+        <a href="tel:+34619792023">
+          <FontAwesomeIcon icon={faPhoneFlip} className="footer-icon" />
+        </a>
+        <a href="https://www.linkedin.com/company/explinsolutions">
+          <FontAwesomeIcon icon={faLinkedinIn} className="footer-icon" />
+        </a>
+      </section>
+    </footer>
+  );
+}
