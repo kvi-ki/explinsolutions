3e80526e8fb4b87a7d1bb61ade258f430244a68f
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-03-24 14:50:39 +0100

    refactor: fix rel for icon link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.png" />
-        <link rel="preload" as="image" type="image/png" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </head>
       <body id="root">{children}</body>
     </html>
