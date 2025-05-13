16b1ec851d80aeeecb2c9d29a8c15e8ec5737200
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-05-13 15:16:24 +0200

    feat: create a form using Input component

--- src/components/contactForm/ContactForm.tsx
+++ src/components/contactForm/ContactForm.tsx
@@ -1,3 +1,49 @@
+import './ContactForm.css';
+import Input from './Input/Input';
+
 export default function ContactForm() {
-  return <></>;
+  return (
+    <form className="p-[6vw] mb-10 bg-lightGray xl:rounded-xl">
+      <h2 className="pt-6 text-center text-gray text-baseFontSize lg:text-largeFontSize lg:pt-12">
+        Ponte en contacto con nosotros
+      </h2>
+      <div>
+        <Input
+          inputData={{
+            name: 'subject',
+            label: 'Asunto',
+            type: 'text',
+            placeholder: 'Introduce el asunto'
+          }}
+        />
+
+        <Input
+          inputData={{
+            name: 'name',
+            label: 'Nombre completo',
+            type: 'text',
+            placeholder: 'Introduce tu nombre'
+          }}
+        />
+
+        <Input
+          inputData={{
+            name: 'email',
+            label: 'Correo electrónico',
+            type: 'email',
+            placeholder: 'Introduce tu correo electrónico'
+          }}
+        />
+
+        <Input
+          inputData={{
+            name: 'message',
+            label: 'Mensaje',
+            type: 'textarea',
+            placeholder: 'Escribe tu mensaje'
+          }}
+        />
+      </div>
+    </form>
+  );
 }
