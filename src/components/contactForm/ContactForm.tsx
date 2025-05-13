40401d2850667956b3f41e7c260bfcf55272c14f
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-05-13 15:39:35 +0200

    style: change form geader size

--- src/components/contactForm/ContactForm.tsx
+++ src/components/contactForm/ContactForm.tsx
@@ -1,22 +1,12 @@
-import './ContactForm.css';
 import Input from './Input/Input';
 
 export default function ContactForm() {
   return (
     <form className="p-[6vw] mb-10 bg-lightGray xl:rounded-xl">
-      <h2 className="pt-6 text-center text-gray text-baseFontSize lg:text-largeFontSize lg:pt-12">
+      <h2 className="pt-6 text-center text-gray text-largeFontSize lg:pt-12">
         Ponte en contacto con nosotros
       </h2>
-      <div>
-        <Input
-          inputData={{
-            name: 'subject',
-            label: 'Asunto',
-            type: 'text',
-            placeholder: 'Introduce el asunto'
-          }}
-        />
-
+      <div className="py-5">
         <Input
           inputData={{
             name: 'name',
@@ -32,6 +22,15 @@ export default function ContactForm() {
             label: 'Correo electrónico',
             type: 'email',
             placeholder: 'Introduce tu correo electrónico'
+          }}
+        />
+
+        <Input
+          inputData={{
+            name: 'subject',
+            label: 'Asunto',
+            type: 'text',
+            placeholder: 'Introduce el asunto'
           }}
         />
 
