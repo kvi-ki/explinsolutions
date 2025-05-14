0227d3604b49fc7af5b754611fd7c5261a98ea06
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-05-14 17:05:32 +0200

    test: should show error when inputs are invalid

--- tests/unit/contactForm.test.tsx
+++ tests/unit/contactForm.test.tsx
@@ -1,7 +1,8 @@
 import ContactForm from '@/components/contactForm/ContactForm';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 describe('ContactForm', () => {
+
   it('should render the contact form fields', () => {
     render(<ContactForm />);
 
@@ -9,5 +10,21 @@ describe('ContactForm', () => {
     expect(screen.getByLabelText('Correo electrónico')).toBeVisible();
     expect(screen.getByLabelText('Asunto')).toBeVisible();
     expect(screen.getByLabelText('Mensaje')).toBeVisible();
+  });
+
+  it('should show error when inputs are invalid', () => {
+    render(<ContactForm />);
+    const submitButton = screen.getByRole('button', { name: 'Enviar' });
+
+    fireEvent.click(submitButton);
+
+    expect(
+      screen.getByRole('textbox', { name: 'Nombre completo' })
+    ).toBeInvalid();
+    expect(
+      screen.getByRole('textbox', { name: 'Correo electrónico' })
+    ).toBeInvalid();
+    expect(screen.getByRole('textbox', { name: 'Asunto' })).toBeInvalid();
+    expect(screen.getByRole('textbox', { name: 'Mensaje' })).toBeInvalid();
   });
 });
