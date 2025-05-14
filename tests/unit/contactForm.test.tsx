368ab2252ecebcebc5efb7e335dad7893f5eeb66
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-05-14 17:40:59 +0200

    test: should submit the form when inputs data is valid

--- tests/unit/contactForm.test.tsx
+++ tests/unit/contactForm.test.tsx
@@ -1,5 +1,15 @@
+jest.mock('@emailjs/browser', () => {
+  return {
+    __esModule: true,
+    default: {
+      sendForm: jest.fn()
+    }
+  };
+});
+
 import ContactForm from '@/components/contactForm/ContactForm';
 import { fireEvent, render, screen } from '@testing-library/react';
+import emailjs from '@emailjs/browser';
 
 describe('ContactForm', () => {
   it('should render the contact form fields', () => {
@@ -32,5 +42,35 @@ describe('ContactForm', () => {
     fireEvent.click(submitButton);
 
     expect(emailField).toBeInvalid();
+  });
+
+  it('should submit the form when inputs data is valid', () => {
+    const mockSendForm = emailjs.sendForm as jest.Mock;
+
+    mockSendForm.mockResolvedValueOnce({
+      status: 200,
+      text: 'OK'
+    });
+
+    render(<ContactForm />);
+    const submitButton = screen.getByRole('button', { name: 'Enviar' });
+    const nameField = screen.getByRole('textbox', { name: 'Nombre completo' });
+    const emailField = screen.getByRole('textbox', { name: 'Correo electrónico' });
+    const subjectField = screen.getByRole('textbox', { name: 'Asunto' });
+    const messageField = screen.getByRole('textbox', { name: 'Mensaje' });
+
+    fireEvent.change(nameField, { target: { value: 'Some Name' } });
+    fireEvent.change(emailField, { target: { value: 'some@email.com' } });
+    fireEvent.change(subjectField, { target: { value: 'Some subject' } });
+    fireEvent.change(messageField, { target: { value: 'Some message' } });
+    fireEvent.click(submitButton);
+
+    expect(mockSendForm).toHaveBeenCalledTimes(1);
+    expect(mockSendForm).toHaveBeenCalledWith(
+      'service_7gulyn7',
+      'template_0obqap3',
+      expect.any(HTMLFormElement),
+      'x5pEv8LMw4oFsjZl9'
+    );
   });
 });
