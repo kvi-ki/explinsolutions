c084060e9294ad8cf2158ee42b08ab20dc995c30
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-05-15 15:10:27 +0200

    style: small sizes fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,24 +9,20 @@ export default function Header() {
   return (
     <>
       <header className="min-w-[8rem] 2xl:w-10/12 2xl:m-auto">
-        <div className="h-8 m-4 flex justify-between items-center 2xl:ml-0 2xl:mr-0">
-          <a aria-label="Go to initial page" className="size-8" href="#root">
+        <div className="h-10 m-4 flex justify-between items-center 2xl:ml-0 2xl:mr-0">
+          <a aria-label="Go to initial page" className="size-10" href="#root">
             <img src="/favicon.png" alt="Explinsolutions label" />
           </a>
 
-          <div className="w-36 flex justify-between md:order-3">
-            <a
-              aria-label="Chat on WhatsApp"
-              href="https://wa.me/34619792023"
-              target="_blank"
-            >
+          <div className="w-40 flex justify-between items-center md:order-3">
+            <a aria-label="Chat on WhatsApp" href="https://wa.me/34619792023" target="_blank">
               <FontAwesomeIcon
                 icon={faWhatsapp}
-                className="text-green size-6 rounded-full hover:bg-green hover:text-white"
+                className="text-green size-7 rounded-full hover:bg-green hover:text-white"
               />
             </a>
             <a href="tel:+34619792023" className="text-gray hover:text-green">
-              <FontAwesomeIcon icon={faPhone} className="text-green pr-1" />
+              <FontAwesomeIcon icon={faPhone} className="text-green pr-1 size-5" />
               619 792 023
             </a>
           </div>
