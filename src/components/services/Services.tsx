b776e57115fa84bd61ae5c2e89af26356ba0866f
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-06-03 11:35:25 +0200

    feat: change the services list appearence

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -10,10 +10,17 @@ export default function Services() {
         <br />
         &nbsp; para sus diversas necesidades
       </h2>
-      <ul className="services-list">
+      {/* <ul className="services-list">
         {data.services.data.map((serviceData) => (
           <Service key={serviceData.name} serviceData={serviceData} />
         ))}
+      </ul> */}
+      <ul className="services-list">
+        {data.services.data.map((serviceData) => (
+          <li key={serviceData.name} className='bg-white border border-green rounded-xl flex flex-col items-center text-darkGray text-center font-lightWeight justify-around p-8 m-2 w-52 h-52'><img src={serviceData.image} alt="service icon" className="size-8 lg:size-12 lg:mb-4" />
+          <h3 className="service-heading">{serviceData.name}</h3></li>
+        )
+        )}
       </ul>
     </section>
   );
