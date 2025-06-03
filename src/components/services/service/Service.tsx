69cd48c3efd1681d1f1ac16835aee6cbadace42e
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-06-03 13:13:47 +0200

    refactor: remove unused toggling of services

--- src/components/services/service/Service.tsx
+++ src/components/services/service/Service.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import './Service.css';
 
 export type ServiceProps = {
@@ -8,50 +7,16 @@ export type ServiceProps = {
 };
 
 export default function Service({ serviceData }: { serviceData: ServiceProps }) {
-  const [isVisible, setIsVisible] = useState(false);
-  const [largeScreenSize, setLargeScreenSize] = useState(false);
-
-  useEffect(() => {
-    const updateScreenSize = () => {
-      setLargeScreenSize(window.innerWidth > 1023 ? true : false);
-    };
-
-    updateScreenSize();
-    window.addEventListener('resize', updateScreenSize);
-
-    return () => window.removeEventListener('resize', updateScreenSize);
-  }, []);
-
-  function toggleList() {
-    setIsVisible((prev) => !prev);
-  }
-
   return (
-    <li
-      className={`${largeScreenSize ? 'service-item-large-screen' : `transition-all duration-500 ease-in-out ${isVisible ? 'service-item-expand' : 'service-item'}`}`}
-    >
+    <li className="service-item-large-screen service-item-expand">
       <img src={serviceData.image} alt="service icon" className="size-8 lg:size-12 lg:mb-4" />
       <h3 className="service-heading">{serviceData.name}</h3>
-
-      {!largeScreenSize && (
-        <p
-          className="text-xs border-b border-b-green cursor-pointer hover:text-green lg:text-smallFontSize"
-          onClick={toggleList}
-          onBlur={toggleList}
-        >
-          Detalles
-        </p>
-      )}
-      <div
-        className={`${largeScreenSize ? 'opacity-100 mt-3' : `transition-all duration-700 ease-in-out ${isVisible ? 'opacity-100' : 'opacity-0'}`}`}
-      >
-        {(isVisible || largeScreenSize) && (
-          <ul className="text-smallFontSize/6 list-disc list-outside pl-6 lg:text-baseFontSize">
-            {serviceData.sections.map((section) => (
-              <li key={section}>{section}</li>
-            ))}
-          </ul>
-        )}
+      <div className="opacity-100 mt-3">
+        <ul className="text-smallFontSize/6 list-disc list-outside pl-6 lg:text-baseFontSize">
+          {serviceData.sections.map((section) => (
+            <li key={section}>{section}</li>
+          ))}
+        </ul>
       </div>
     </li>
   );
