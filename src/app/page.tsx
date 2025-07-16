ed2d04979e66c59fea7c5babd3e78a70da429e13
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-07-16 13:15:02 +0200

    feat: use banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,17 @@ import Description from '@/components/description/Description';
 import Services from '@/components/services/Services';
 import Values from '@/components/values/Values';
 import React from 'react';
+import Image from 'next/image';
 import { StrictMode } from 'react';
 
 export default function Page() {
   return (
     <StrictMode>
-      <div className="bg-[url(/assets/electric_tower.jpg)] heading-wrapper">
+      <Image src="/banner.jpg" alt={''} width={1500} height={384} />
+      {/* <div className="bg-[url(/assets/electric_tower.jpg)] heading-wrapper">
     <h1 className="heading"><span className="font-bold">explin</span>solutions<span className="text-green">_</span></h1>
     <Values />
-  </div>
+  </div> */}
       <Description />
       <Services />
       <Contacts />
