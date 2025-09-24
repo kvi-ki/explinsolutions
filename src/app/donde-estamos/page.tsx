c88d6ecab506573ee78e9b49e8af22507c8b3d22
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-09-24 17:10:43 +0200

    refactor: remove unused page

--- src/app/donde-estamos/page.tsx
+++
@@ -1,9 +0,0 @@
-import Contacts from '@/components/contacts/Contacts';
-
-export default function ContactDetailsPage() {
-  return (
-    <section className="bg-lightGray py-8 mb-10 xl:rounded-xl">
-      <Contacts />
-    </section>
-  );
-}
