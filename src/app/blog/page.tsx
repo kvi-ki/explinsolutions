9462ca8862892cc29ad1c401fc7729b03536ddf4
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-10-01 20:04:51 +0200

    style: update layouts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,8 +9,8 @@ export default function Blog() {
     <div className="bg-lightGray pt-10 my-10 flex flex-col justify-center items-center xl:rounded-xl">
       <h2 className="text-blackColor text-2xl lg:text-4xl">Blog</h2>
       <AnimatedService>
-        <ul className="p-10 flex flex-wrap gap-10">
-          <li className="shadow-[0_0_10px_rgb(0,0,0,0.09)] rounded-xl transition-transform ease-out duration-300 lg:w-md lg:h-[30rem] hover:scale-105 hover:cursor-pointer">
+        <ul className="p-10 flex flex-wrap justify-center gap-10">
+          <li className="shadow-[0_0_10px_rgb(0,0,0,0.09)] rounded-xl transition-transform ease-out duration-300 md:w-sm lg:h-[30rem] hover:scale-105 hover:cursor-pointer">
             <Link
               href="/blog/first-post"
               className="p-8 flex flex-col items-center"
@@ -32,7 +32,7 @@ export default function Blog() {
             </Link>
           </li>
 
-          <li className="shadow-[0_0_10px_rgb(0,0,0,0.09)] rounded-xl transition-transform ease-out duration-300 md:w-md lg:h-[30rem] hover:scale-105 hover:cursor-pointer">
+          <li className="shadow-[0_0_10px_rgb(0,0,0,0.09)] rounded-xl transition-transform ease-out duration-300 md:w-sm lg:h-[30rem] hover:scale-105 hover:cursor-pointer">
             <Link
               href="/blog/first-post"
               className="p-8 flex flex-col items-center"
@@ -54,7 +54,7 @@ export default function Blog() {
             </Link>
           </li>
 
-          <li className="shadow-[0_0_10px_rgb(0,0,0,0.09)] rounded-xl transition-transform ease-out duration-300 lg:w-md lg:h-[30rem] hover:scale-105 hover:cursor-pointer">
+          <li className="shadow-[0_0_10px_rgb(0,0,0,0.09)] rounded-xl transition-transform ease-out duration-300 md:w-sm lg:h-[30rem] hover:scale-105 hover:cursor-pointer">
             <Link
               href="/blog/first-post"
               className="p-8 flex flex-col items-center"
