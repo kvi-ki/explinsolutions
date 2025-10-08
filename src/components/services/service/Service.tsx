e7ae6dab0cfed711827e36d9e50ac7be51154141
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-10-08 11:54:49 +0200

    style: change the services page layout

--- src/components/services/service/Service.tsx
+++ src/components/services/service/Service.tsx
@@ -11,60 +11,70 @@ export type ServiceProps = {
   };
 };
 
-export default function Service({
-  serviceData,
-  index
-}: {
-  serviceData: ServiceProps;
-  index: number;
-}) {
+export default function Service({ serviceData }: { serviceData: ServiceProps }) {
   return (
     <AnimatedService>
       <div
         id={slugify(serviceData.name)}
-        className={`scroll-mt-20 border-b-2 border-accent bg-whiteColor text-text font-light p-14 md:flex md:justify-around md:items-center xl:scroll-mt-16 ${index % 2 !== 0 ? 'md:flex-row-reverse' : ''}`}
+        className={`scroll-mt-20 border-b-2 border-accent bg-whiteColor text-text text-justify  text-base font-light p-14 xl:scroll-mt-16`}
       >
-        <div className="md:w-2/5">
-          <div className="flex flex-col gap-2">
-            <img
-              src={serviceData.image}
-              alt="service icon"
-              className="size-12 mb-4 mx-auto"
-            />
-            <h3 className="text-center text-wrap font-bold text-2xl mb-4">
-              {serviceData.name}
-            </h3>
-          </div>
-          <ul className="text-base flex flex-col gap-8">
-            <li key={4} className="my-4 text-justify space-y-4">
+        <div className="flex flex-col gap-2 pb-10">
+          <img
+            src={serviceData.image}
+            alt="service icon"
+            className="size-16 mb-4 mx-auto"
+          />
+          <h3 className="text-center text-wrap font-bold text-2xl mb-4">
+            {serviceData.name}
+          </h3>
+        </div>
+
+        <ul className="flex flex-col gap-20">
+          <li
+            key={4}
+            className="flex flex-col items-center justify-center gap-10 md:flex-row lg:gap-20"
+          >
+            <div className="flex flex-col gap-8 lg:w-2/5">
               <h3 className="text-xl font-medium">
                 {serviceData.sections.firstSection.title}
               </h3>
-              <ul>
+              <ul className="space-y-4">
                 {serviceData.sections.firstSection.description.map((item) => (
                   <li key={item.slice(0, 8)}>{item}</li>
                 ))}
               </ul>
-            </li>
-            <li key={5} className="my-4 text-justify space-y-4">
+            </div>
+            <motion.img
+              src="/service-images/title2.jpg"
+              alt="machine"
+              className="bg-lightGray rounded-xl w-full md:w-1/4 md:m-4"
+              whileHover={{ scale: 1.06 }}
+              transition={{ duration: 0.3 }}
+            />
+          </li>
+          <li
+            key={5}
+            className="flex flex-col items-center justify-center gap-10 md:flex-row-reverse lg:gap-20"
+          >
+            <div className="flex flex-col gap-8 lg:w-2/5">
               <h3 className="text-xl font-medium">
                 {serviceData.sections.secondSection.title}
               </h3>
-              <ul>
+              <ul className="space-y-4">
                 {serviceData.sections.secondSection.description.map((item) => (
                   <li key={item.slice(0, 8)}>{item}</li>
                 ))}
               </ul>
-            </li>
-          </ul>
-        </div>
-        <motion.img
-          src="/service-images/title2.jpg"
-          alt="machine"
-          className="bg-lightGray rounded-xl w-full md:w-2/5 md:m-4"
-          whileHover={{ scale: 1.06 }}
-          transition={{ duration: 0.3 }}
-        />
+            </div>
+            <motion.img
+              src="/service-images/title2.jpg"
+              alt="machine"
+              className="bg-lightGray rounded-xl w-full md:w-1/4 md:m-4"
+              whileHover={{ scale: 1.06 }}
+              transition={{ duration: 0.3 }}
+            />
+          </li>
+        </ul>
       </div>
     </AnimatedService>
   );
