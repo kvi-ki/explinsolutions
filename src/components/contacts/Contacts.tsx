5f4ec002b3bcc01b4f0b73c64573b98992696cc5
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-10-14 12:42:50 +0200

    feat: add second header to contact details section

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -5,29 +5,34 @@ export default function Contacts() {
   return (
     <section
       id="contacts"
-      className="h-[50rem] mb-10 flex flex-col items-center justify-around lg:h-[30rem] lg:flex-row"
+      className="mb-30 flex flex-col items-center justify-around lg:h-[30rem]"
     >
-      <ul>
-        {data.contacts.data.map((contact) => (
-          <Contact
-            key={contact.link}
-            contact={{
-              icon: contact.icon,
-              text: contact.text,
-              link: contact.link
-            }}
-          />
-        ))}
-      </ul>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5973.584035260995!2d2.4197296999999995!3d41.5304456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4b50057db099d%3A0x4cfeddb863fec1fc!2sExplinsolutions!5e0!3m2!1sen!2ses!4v1743757698960!5m2!1sen!2ses"
-        title="Ubicación de explinsolutions_ en Google Maps"
-        className="w-full h-full p-4 lg:max-w-[40rem]"
-        style={{ border: 0 }}
-        allowFullScreen={true}
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+      <h2 className="pb-6 text-center text-blackColor text-3xl lg:pt-12 lg:text-4xl">
+        Dónde Estamos
+      </h2>
+      <div className="w-full mb-10 flex flex-col items-center justify-around lg:h-[30rem] lg:flex-row">
+        <ul>
+          {data.contacts.data.map((contact) => (
+            <Contact
+              key={contact.link}
+              contact={{
+                icon: contact.icon,
+                text: contact.text,
+                link: contact.link
+              }}
+            />
+          ))}
+        </ul>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5973.584035260995!2d2.4197296999999995!3d41.5304456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4b50057db099d%3A0x4cfeddb863fec1fc!2sExplinsolutions!5e0!3m2!1sen!2ses!4v1743757698960!5m2!1sen!2ses"
+          title="Ubicación de explinsolutions_ en Google Maps"
+          className="w-full h-full p-4 lg:max-w-[40rem]"
+          style={{ border: 0 }}
+          allowFullScreen={true}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
     </section>
   );
 }
