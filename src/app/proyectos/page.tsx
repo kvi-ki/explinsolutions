f16644502fedcef2d562e5046f3ee6d3306af37d
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-10-30 16:02:47 +0100

    refactor: use PostCard in ProjectsPage

--- src/app/proyectos/page.tsx
+++ src/app/proyectos/page.tsx
@@ -1,80 +1,34 @@
 'use client';
 
-import Link from 'next/link';
-
 import AnimatedService from '@/utils/AnimatedService';
 
+import projectsList from '@/data/projects.json';
+import PostCard from '@/components/postCard/PostCard';
+
 export default function ProjectsPage() {
+  const formatPath = (title: string) => {
+    return title.toLowerCase().replace(/\s+/g, '-');
+  };
+
   return (
     <div className="bg-lightGray pt-10 my-10 flex flex-col justify-center items-center xl:rounded-xl">
       <h2 className="text-blackColor text-3xl lg:text-4xl">Proyectos</h2>
       <AnimatedService>
         <ul className="p-10 flex flex-wrap justify-center gap-10">
-          <li className="shadow-[0_0_10px_rgb(0,0,0,0.09)] rounded-xl transition-transform ease-out duration-300 md:w-sm lg:h-[30rem] hover:scale-105 hover:cursor-pointer">
-            <Link
-              href="/proyectos/proyecto"
-              className="p-8 flex flex-col items-center"
-              aria-label="Go to first post page"
+          {projectsList.map((project) => (
+            <li
+              key={project.title}
+              className="shadow-[0_0_10px_rgb(0,0,0,0.09)] rounded-xl transition-transform ease-out duration-300 md:w-sm lg:h-[30rem] hover:scale-105 hover:cursor-pointer"
             >
-              <h3 className="text-2xl">Nombre del Proyecto</h3>
-              <div className="flex flex-col justify-center items-center gap-8">
-                <img
-                  src="/images/projects/example.jpg"
-                  className="w-80 h-60 object-cover"
-                  alt="Post name image"
-                ></img>
-                <p className="text-text text-center">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting
-                  industry. Lorem Ipsum has been the industry's standard dummy text
-                  ever since the 1500s...
-                </p>
-              </div>
-            </Link>
-          </li>
-
-          <li className="shadow-[0_0_10px_rgb(0,0,0,0.09)] rounded-xl transition-transform ease-out duration-300 md:w-sm lg:h-[30rem] hover:scale-105 hover:cursor-pointer">
-            <Link
-              href="/proyectos/proyecto"
-              className="p-8 flex flex-col items-center"
-              aria-label="Go to first post page"
-            >
-              <h3 className="text-2xl">Nombre del Proyecto</h3>
-              <div className="flex flex-col justify-center items-center gap-8">
-                <img
-                  src="/images/projects/example.jpg"
-                  className="w-80 h-60 object-cover"
-                  alt="Post name image"
-                ></img>
-                <p className="text-text text-center">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting
-                  industry. Lorem Ipsum has been the industry's standard dummy text
-                  ever since the 1500s...
-                </p>
-              </div>
-            </Link>
-          </li>
-
-          <li className="shadow-[0_0_10px_rgb(0,0,0,0.09)] rounded-xl transition-transform ease-out duration-300 md:w-sm lg:h-[30rem] hover:scale-105 hover:cursor-pointer">
-            <Link
-              href="/proyectos/proyecto"
-              className="p-8 flex flex-col items-center"
-              aria-label="Go to first post page"
-            >
-              <h3 className="text-2xl">Nombre del Proyecto</h3>
-              <div className="flex flex-col justify-center items-center gap-8">
-                <img
-                  src="/images/projects/example.jpg"
-                  className="w-80 h-60 object-cover"
-                  alt="Post name image"
-                ></img>
-                <p className="text-text text-center">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting
-                  industry. Lorem Ipsum has been the industry's standard dummy text
-                  ever since the 1500s...
-                </p>
-              </div>
-            </Link>
-          </li>
+              <PostCard
+                title={project.title}
+                imageSource={project.images[0].source}
+                imageAlt={project.images[0].alt}
+                description={project.paragraps[0]}
+                link={`/proyectos/${formatPath(project.title)}`}
+              />
+            </li>
+          ))}
         </ul>
       </AnimatedService>
     </div>
