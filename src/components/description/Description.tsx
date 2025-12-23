2ca4ecfc65a6ea4a999dc4d65d758ae93cc51fca
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-12-23 12:32:23 +0100

    style: update the font-size and text of Description

--- src/components/description/Description.tsx
+++ src/components/description/Description.tsx
@@ -1,10 +1,7 @@
 export default function Description() {
   return (
     <section className="p-[6vw] xl:p-30">
-      <h2 className="text-right text-2xl lg:text-3xl mb-3">
-        Ingeniería industrial especializada en automatización y sistemas eléctricos
-      </h2>
-      <p className="text-lg font-light text-text pt-10 lg:text-justify lg:text-2xl">
+      <p className="text-right text-text font-light text-xl lg:text-3xl">
         Diseñamos, programamos, fabricamos e implantamos soluciones técnicas que
         optimizan procesos, mejoran la eficiencia y aportan valor real a cada
         cliente, ya sea en proyectos llave en mano o en necesidades específicas.
